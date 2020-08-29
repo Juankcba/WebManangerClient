c074e648c1942d46c79d7421cb539a1f1a2acf83
juankcba <juangblade@hotmail.com>
2020-08-28 22:44:38 -0300

    master

---
+++ build/precache-manifest.a989bd8e5140dd9ecc9ecbb907a096fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9083b3b37a4f1f7e5af311098b382206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec502f4e8eab3ecde7b",
+    "url": "/static/css/main.f9a2c2db.chunk.css"
+  },
+  {
+    "revision": "6e594ee21d720e555c8d",
+    "url": "/static/js/2.32d49185.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.32d49185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec502f4e8eab3ecde7b",
+    "url": "/static/js/main.2b6a0943.chunk.js"
+  },
+  {
+    "revision": "d798b35c19d3a6a275f1",
+    "url": "/static/js/runtime-main.2122a57e.js"
+  }
+]);
